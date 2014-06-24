4c3b4a7260099d9cd8ffc107483025975c641961
Al Capwn <gadonj18@live.com>
2014-06-24 08:52:56 -0600

    Added high score dictionary that reads/writes to PlayerPrefs.

--- Assets/Scripts/ApplicationLogic.cs
+++ Assets/Scripts/ApplicationLogic.cs
@@ -6,8 +6,11 @@ using System.Collections.Generic;
 public class ApplicationLogic : MonoBehaviour {
 	private string playerName; //Not sure why I even need this, I guess for high scores?
 	private long currentScore; //Will carry through to the scoreboard
-	private ushort currentLevel; //Level being played
-	private ushort unlockedLevel; //Highest level user has unlocked (Default = 1)
+	private int currentLevel; //Level being played
+	private int unlockedLevel; //Highest level user has unlocked (Default = 1)
+	public Dictionary<int, long> highScores;
+
+	public int numLevels; //Stores the number of levels in the game
 
 	void Start() {
 		//Used to persist object across scenes
@@ -19,9 +22,17 @@ public class ApplicationLogic : MonoBehaviour {
 		#else
 			this.PlayerName = PlayerPrefs.GetString("PlayerName", System.Environment.UserName);
 		#endif
+
 		this.CurrentScore = 0;
 		this.CurrentLevel = 0;
-		this.UnlockedLevel = (ushort)PlayerPrefs.GetInt("UnlockedLevel", 1);
+		this.UnlockedLevel = (int)PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+		this.highScores = new Dictionary<int, long>();
+
+		//Grab any saved high scores
+		for(int i = 1; i <= this.numLevels; i++) {
+			this.highScores[i] = (long)PlayerPrefs.GetInt("HighScoreLevel" + i, 0);
+		}
 	}
 
 	//On button click from start screen
@@ -30,7 +41,7 @@ public class ApplicationLogic : MonoBehaviour {
 	}
 
 	//On button click from start screen
-	public void StartGame(ushort levelNum = 0) {
+	public void StartGame(int levelNum = 0) {
 		if(levelNum > 0) this.CurrentLevel = levelNum;
 		this.CurrentScore = 0;
 		Application.LoadLevel("MainGameScene");
@@ -46,7 +57,10 @@ public class ApplicationLogic : MonoBehaviour {
 	}
 
 	public void WinGame() {
-		//TODO: Keep scores. Should the high score screen be shown after each level?
+		if(this.currentScore > this.highScores[this.currentLevel]) {
+			this.highScores[this.currentLevel] = this.currentScore;
+			PlayerPrefs.SetInt("HighScoreLevel" + this.currentLevel, (int)this.currentScore);
+		}
 		Invoke("NextLevel", 3.0f);
 	}
 
@@ -66,13 +80,14 @@ public class ApplicationLogic : MonoBehaviour {
 		private set { this.currentScore = value; }
 	}
 	
-	public ushort CurrentLevel {
+	public int CurrentLevel {
 		get { return this.currentLevel; }
 		private set { this.currentLevel = value; }
 	}
 	
-	public ushort UnlockedLevel {
+	public int UnlockedLevel {
 		get { return this.unlockedLevel; }
 		private set { this.unlockedLevel = value; }
 	}
+
 }
