10a1d8a5f5bfb6b90eb47ff6dc6afc946b8d56f2
Al Capwn <gadonj18@live.com>
2014-06-24 08:53:55 -0600

    Changed to only keep 1 level loaded at a time. Finished the start button.

--- Assets/Scripts/LevelSelectLogic.cs
+++ Assets/Scripts/LevelSelectLogic.cs
@@ -3,55 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LevelSelectLogic : MonoBehaviour {
+	public ApplicationLogic logic;
 	public SpriteRenderer PreviewImage;
 
-	private ushort currentLevel;
-	private Dictionary<ushort, BaseLevel> levels = new Dictionary<ushort, BaseLevel>();
+	private int currentLevelNum;
+	private BaseLevel currentLevel;
 
 	void Start () {
-		this.LoadLevels();
-		this.CurrentLevel = 1;
+		this.logic = GameObject.Find("_ApplicationLogic").GetComponent<ApplicationLogic>();
+		this.currentLevelNum = 1;
 		this.ShowLevel();
 	}
 
-	private void LoadLevels() {
-		for(ushort i = 1; i <= 100; i++) {
-			Object resource = Resources.Load("Level" + i, typeof(BaseLevel));
-			if(!resource) break;
-			levels.Add(i, (BaseLevel)Instantiate(resource));
-		}
-	}
-
 	private void ShowLevel() {
-		if(!levels.ContainsKey(this.CurrentLevel)) return;
-		this.PreviewImage.sprite = levels[this.CurrentLevel].background;
+		if(this.currentLevelNum < 0 || this.currentLevelNum > this.logic.numLevels) return;
+		if(this.currentLevel) Destroy(this.currentLevel.gameObject);
+		this.currentLevel = (BaseLevel)Instantiate(Resources.Load("Level" + this.currentLevelNum, typeof(BaseLevel)));
+		this.PreviewImage.sprite = this.currentLevel.background;
 		//TODO: Scale image properly... too lazy right now
 	}
 
 	void OnGUI() {
-		GUI.skin.label.fontSize = 30;
-		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
-		GUI.Label(new Rect(Screen.width / 2 - 70, 20, 140, 50), "Level " + this.CurrentLevel);
-		GUI.skin.label.alignment = TextAnchor.MiddleLeft;
-		GUI.Label(new Rect(Screen.width / 2 - this.PreviewImage.sprite.rect.x / 2, 60, 200, 50), "Target: " + this.levels[this.CurrentLevel].targetHits + " hits");
-		if(this.CurrentLevel == 1) GUI.enabled = false;
-		if(GUI.Button(new Rect(10, Screen.height / 2, 50, 50), "<")) {
-			this.CurrentLevel--;
-			this.ShowLevel();
+		if(this.currentLevel) {
+			GUI.skin.label.fontSize = 30;
+			GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+			GUI.Label(new Rect(Screen.width / 2 - 70, 20, 140, 50), "Level " + this.currentLevelNum);
+			GUI.skin.label.alignment = TextAnchor.MiddleLeft;
+			GUI.Label(new Rect(Screen.width / 2 - this.PreviewImage.sprite.rect.x / 2, 60, 200, 50), "Target: " + this.currentLevel.targetHits + " hits");
+			if(this.currentLevelNum == 1)
+				GUI.enabled = false;
+			if(GUI.Button(new Rect(10, Screen.height / 2, 50, 50), "<")) {
+				this.currentLevelNum--;
+				this.ShowLevel();
+			}
+			GUI.enabled = true;
+			if(this.currentLevelNum == this.logic.numLevels)
+				GUI.enabled = false;
+			if(GUI.Button(new Rect(Screen.width - 60, Screen.height / 2, 50, 50), ">")) {
+				this.currentLevelNum++;
+				this.ShowLevel();
+			}
+			GUI.enabled = true;
+			if(GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50), "Play!")) {
+				this.currentLevelNum = 1;
+				Destroy(this.currentLevel);
+				this.logic.StartGame(this.currentLevelNum);
+			}
 		}
-		GUI.enabled = true;
-		if(this.CurrentLevel == this.levels.Count) GUI.enabled = false;
-		if(GUI.Button(new Rect(Screen.width - 60, Screen.height / 2, 50, 50), ">")) {
-			this.CurrentLevel++;
-			this.ShowLevel();
-		}
-		GUI.enabled = true;
-		if(GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50), "Play!")) {
-		}
-	}
-	
-	public ushort CurrentLevel {
-		get { return this.currentLevel; }
-		private set { this.currentLevel = value; }
 	}
 }
